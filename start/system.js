9cbb1266dfe18a6f0abcaf864977006788cb8a10
ꓘIuuR <kyuurzy@gmail.com>
2025-02-02 12:09:24 +0800

    Create system.js

---
+++ start/system.js
@@ -0,0 +1,266 @@
+/**
+ * github : https://github.com/kiuur
+ * youtube : https://youtube.com/@kyuurzy
+*/
+
+require('../settings/config');
+
+const fs = require('fs');
+const axios = require('axios');
+const chalk = require("chalk");
+const jimp = require("jimp")
+const util = require("util");
+const moment = require("moment-timezone");
+const path = require("path")
+const os = require('os');
+
+const {
+    spawn, 
+    exec,
+    execSync 
+   } = require('child_process');
+
+const {
+    default:
+    baileys,
+    getContentType, 
+   } = require("@whiskeysockets/baileys");
+
+module.exports = client = async (client, m, chatUpdate, store) => {
+    try {
+        const body = (
+            m.mtype === "conversation" ? m.message.conversation :
+            m.mtype === "imageMessage" ? m.message.imageMessage.caption :
+            m.mtype === "videoMessage" ? m.message.videoMessage.caption :
+            m.mtype === "extendedTextMessage" ? m.message.extendedTextMessage.text :
+            m.mtype === "buttonsResponseMessage" ? m.message.buttonsResponseMessage.selectedButtonId :
+            m.mtype === "listResponseMessage" ? m.message.listResponseMessage.singleSelectReply.selectedRowId :
+            m.mtype === "templateButtonReplyMessage" ? m.message.templateButtonReplyMessage.selectedId :
+            m.mtype === "interactiveResponseMessage" ? JSON.parse(m.msg.nativeFlowResponseMessage.paramsJson).id :
+            m.mtype === "templateButtonReplyMessage" ? m.msg.selectedId :
+            m.mtype === "messageContextInfo" ? m.message.buttonsResponseMessage?.selectedButtonId || m.message.listResponseMessage?.singleSelectReply.selectedRowId || m.text : "");
+        
+        const sender = m.key.fromMe ? client.user.id.split(":")[0] + "@s.whatsapp.net" || client.user.id
+: m.key.participant || m.key.remoteJid;
+        
+        const senderNumber = sender.split('@')[0];
+        const budy = (typeof m.text === 'string' ? m.text : '');
+        const prefa = ["", "!", ".", ",", "🐤", "🗿"];
+        const prefix = /^[°zZ#$@+,.?=''():√%!¢£¥€π¤ΠΦ&><™©®Δ^βα¦|/\\©^]/.test(body) ? body.match(/^[°zZ#$@+,.?=''():√%¢£¥€π¤ΠΦ&><!™©®Δ^βα¦|/\\©^]/gi) : '.';
+        const from = m.key.remoteJid;
+        const isGroup = from.endsWith("@g.us");
+
+        const kontributor = JSON.parse(fs.readFileSync('./start/lib/database/owner.json'));
+        const botNumber = await client.decodeJid(client.user.id);
+        const Access = [botNumber, ...kontributor, ...global.owner].map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender)
+        
+        const isCmd = body.startsWith(prefix);
+        const command = body.slice(1).trim().split(/ +/).shift().toLowerCase();
+        const args = body.trim().split(/ +/).slice(1);
+        const pushname = m.pushName || "No Name";
+        const text = q = args.join(" ");
+        const quoted = m.quoted ? m.quoted : m;
+        const mime = (quoted.msg || quoted).mimetype || '';
+        const qmsg = (quoted.msg || quoted);
+        const isMedia = /image|video|sticker|audio/.test(mime);
+
+        const groupMetadata = isGroup ? await client.groupMetadata(m.chat).catch((e) => {}) : "";
+        const groupOwner = isGroup ? groupMetadata.owner : "";
+        const groupName = m.isGroup ? groupMetadata.subject : "";
+        const participants = isGroup ? await groupMetadata.participants : "";
+        const groupAdmins = isGroup ? await participants.filter((v) => v.admin !== null).map((v) => v.id) : "";
+        const groupMembers = isGroup ? groupMetadata.participants : "";
+        const isGroupAdmins = isGroup ? groupAdmins.includes(m.sender) : false;
+        const isBotGroupAdmins = isGroup ? groupAdmins.includes(botNumber) : false;
+        const isBotAdmins = isGroup ? groupAdmins.includes(botNumber) : false;
+        const isAdmins = isGroup ? groupAdmins.includes(m.sender) : false;
+        
+        const {
+            smsg,
+            resize,
+            fetchJson, 
+            sleep,
+            formatSize
+            } = require('./lib/myfunction');
+        
+        let cihuy = fs.readFileSync('./start/lib/media/rimuru.png')
+       
+        if (m.message) {
+            console.log('\x1b[30m--------------------\x1b[0m');
+            console.log(chalk.bgHex("#e74c3c").bold(`▢ New Message`));
+            console.log(
+                chalk.bgHex("#00FF00").black(
+                    `   ⌬ Tanggal: ${new Date().toLocaleString()} \n` +
+                    `   ⌬ Pesan: ${m.body || m.mtype} \n` +
+                    `   ⌬ Pengirim: ${m.pushname} \n` +
+                    `   ⌬ JID: ${senderNumber}`
+                )
+            );
+            
+            if (m.isGroup) {
+                console.log(
+                    chalk.bgHex("#00FF00").black(
+                        `   ⌬ Grup: ${groupName} \n` +
+                        `   ⌬ GroupJid: ${m.chat}`
+                    )
+                );
+            }
+            console.log();
+        }
+        
+        const reaction = async (jidss, emoji) => {
+            client.sendMessage(jidss, {
+                react: {
+                    text: emoji,
+                    key: m.key 
+                } 
+            })
+        };
+        
+        async function reply(text) {
+            client.sendMessage(m.chat, {
+                text: text,
+                contextInfo: {
+                    mentionedJid: [sender],
+                    externalAdReply: {
+                        title: "— rimuru tempest",
+                        body: "— kiuur senpai",
+                        thumbnailUrl: "https://files.catbox.moe/wemxs3.jpg",
+                        sourceUrl: 'https://github.com/kiuur/tempest',
+                        renderLargerThumbnail: false,
+                    }
+                }
+            }, { quoted: m })
+        }
+        
+        const pluginsLoader = async (directory) => {
+            let plugins = [];
+            const folders = fs.readdirSync(directory);
+            folders.forEach(file => {
+                const filePath = path.join(directory, file);
+                if (filePath.endsWith(".js")) {
+                    try {
+                        const resolvedPath = require.resolve(filePath);
+                        if (require.cache[resolvedPath]) {
+                            delete require.cache[resolvedPath];
+                        }
+                        const plugin = require(filePath);
+                        plugins.push(plugin);
+                    } catch (error) {
+                        console.log(`${filePath}:`, error);
+                    }
+                }
+            });
+            return plugins;
+        };
+
+        const pluginsDisable = true;
+        const plugins = await pluginsLoader(path.resolve(__dirname, "../helper"));
+        const plug = { client, prefix, command, reply, text, reaction, isGroup: m.isGroup, isPrivate: !m.isGroup };
+
+        for (let plugin of plugins) {
+            if (plugin.command.find(e => e == command.toLowerCase())) {
+                if (plugin.owner && !Access) {
+                    return reply(mess.owner);
+                }
+                
+                if (plugin.group && !plug.isGroup) {
+                    return m.reply(mess.group);
+                }
+                
+                if (plugin.private && !plug.isPrivate) {
+                    return m.reply(mess.private);
+                }
+
+                if (typeof plugin !== "function") return;
+                await plugin(m, plug);
+            }
+        }
+        
+        if (!pluginsDisable) return;  
+        
+        switch (command) {
+            
+            case "menu":{
+                const totalMem = os.totalmem();
+                const freeMem = os.freemem();
+                const usedMem = totalMem - freeMem;
+                const formattedUsedMem = formatSize(usedMem);
+                const formattedTotalMem = formatSize(totalMem);
+                let mbut = `hi ${pushname}, i am automated system (WhatsApp bot) that can help to do something search and get data/informasi only through WhatsApp 
+
+information:
+ ▢ your ID: 6816288495
+ ▢ username: @kyuucode
+ ▢ RAM: ${formattedUsedMem} / ${formattedTotalMem}
+
+commands:
+ ▢ ${prefix}tiktok
+ ▢ ${prefix}igdl`
+                client.sendMessage(m.chat, {
+                    document: fs.readFileSync("./package.json"),
+                    fileName: "— rimuru tempest",
+                    mimetype: "application/pdf",
+                    fileLength: 99999,
+                    pageCount: 666,
+                    caption: mbut,
+                    contextInfo: {
+                        forwardingScore: 999,
+                        isForwarded: true,
+                        mentionedJid: [sender],
+                        forwardedNewsletterMessageInfo: {
+                            newsletterName: "— rimuru tempest",
+                            newsletterJid: `120363384742227772@newsletter`,
+                        },
+                        externalAdReply: {  
+                            title: "— rimuru tempest", 
+                            body: "rimuru bjiirr",
+                            thumbnailUrl: `https://files.catbox.moe/wemxs3.jpg`,
+                            sourceUrl: "https://github.com/kiuur/tempest", 
+                            mediaType: 1,
+                            renderLargerThumbnail: true
+                        }
+                    }
+                }, { quoted: m })
+            };
+            break;
+ 
+            default:
+                
+                if (budy.startsWith('>')) {
+                    if (!Access) return;
+                    try {
+                        let evaled = await eval(budy.slice(2));
+                        if (typeof evaled !== 'string') evaled = require('util').inspect(evaled);
+                        await m.reply(evaled);
+                    } catch (err) {
+                        m.reply(String(err));
+                    }
+                }
+        
+                if (budy.startsWith('<')) {
+                    if (!Access) return
+                    let kode = budy.trim().split(/ +/)[0]
+                    let teks
+                    try {
+                        teks = await eval(`(async () => { ${kode == ">>" ? "return" : ""} ${q}})()`)
+                    } catch (e) {
+                        teks = e
+                    } finally {
+                        await m.reply(require('util').format(teks))
+                    }
+                }
+        
+        }
+    } catch (err) {
+        console.log(require("util").format(err));
+    }
+};
+
+let file = require.resolve(__filename);
+require('fs').watchFile(file, () => {
+    require('fs').unwatchFile(file);
+    console.log('\x1b[0;32m' + __filename + ' \x1b[1;32mupdated!\x1b[0m');
+    delete require.cache[file];
+    require(file);
+});

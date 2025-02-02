2ca7aefc0e3e156830bbf01b6a099d29a034cf20
ꓘIuuR <kyuurzy@gmail.com>
2025-02-02 11:55:15 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+
+global.owner = "62"
+global.status = false 
+
+global.mess = {
+    owner: "no, this is for owners only",
+    group: "this is for groups only",
+    private: "this is specifically for private chat"
+}
+
+let file = require.resolve(__filename)
+require('fs').watchFile(file, () => {
+  require('fs').unwatchFile(file)
+  console.log('\x1b[0;32m'+__filename+' \x1b[1;32mupdated!\x1b[0m')
+  delete require.cache[file]
+  require(file)
+})

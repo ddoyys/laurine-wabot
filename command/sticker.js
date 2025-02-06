325fdd89bc40ad92f539588a9caaf027da525142
ꓘIuuR <kyuurzy@gmail.com>
2025-02-06 12:42:53 +0800

    Update sticker.js

--- command/sticker.js
+++ command/sticker.js
@@ -12,7 +12,7 @@ let handler = async (m, { client, text, reply, quoted, mime, prefix, command }) 
             });
             await fs.unlinkSync(encmedia);
         } else if (/video/.test(mime)) {
-            if ((quoted?.msg || quoted)?.seconds > 10) return reply('Durasi video maksimal 10 detik!')
+            if ((quoted?.msg || quoted)?.seconds > 10) return reply('\ndurasi maksimal 10 detik\n')
                 const media = await quoted.download();
                 let encmedia = await client.sendVideoAsSticker(m.chat, media, m, {
                     packname: packname,
@@ -20,11 +20,11 @@ let handler = async (m, { client, text, reply, quoted, mime, prefix, command }) 
                 });
             await fs.unlinkSync(encmedia);
         } else {
-                return reply('Kirim gambar/video dengan caption .s (video durasi 1-10 detik)');
+                return reply('\n*ex:* reply image/video ${prefix + command}\n');
         }
     } catch (error) {
         console.error(error);
-        return reply('Terjadi kesalahan saat memproses media. Coba lagi.');
+        return reply('error');
     }
 }
 

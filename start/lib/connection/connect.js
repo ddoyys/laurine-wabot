2aa24940fe9214e9aa11bb8664e117c511373230
ꓘIuuR <kyuurzy@gmail.com>
2025-03-17 12:11:39 +0800

    Update connect.js

--- start/lib/connection/connect.js
+++ start/lib/connection/connect.js
@@ -1,3 +1,11 @@
+
+/**
+ * GitHub   : https://github.com/kiuur
+ * YouTube  : https://youtube.com/@kyuurzy
+ * Rest API : https://laurine.site
+ * Telegram : https://kyuucode.t.me
+ */
+
 const konek = async ({
     client,
     update,

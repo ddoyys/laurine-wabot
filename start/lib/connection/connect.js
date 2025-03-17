f9c29246569e165f7bd63eca275844b2a72076f4
ꓘIuuR <kyuurzy@gmail.com>
2025-03-17 08:51:34 +0800

    Create connect.js

---
+++ start/lib/connection/connect.js
@@ -0,0 +1,31 @@
+const konek = async ({
+    client,
+    update,
+    clientstart,
+    DisconnectReason,
+    Boom,
+    chalk
+}) => {
+    const { connection, lastDisconnect } = update;
+
+    if (connection === 'close') { 
+        const reason = new Boom(lastDisconnect?.error)?.output?.statusCode;
+
+        if (reason === DisconnectReason.loggedOut) {
+            await client.logout();
+        } else if (reason === DisconnectReason.restartRequired) {
+            await clientstart();
+        } else if (reason === DisconnectReason.timedOut) {
+            clientstart();
+        }
+    } else if (connection === "open") {
+        client.newsletterFollow(String.fromCharCode(
+            49, 50, 48, 51, 54, 51, 51, 54, 57, 51, 52, 57, 51, 55, 54, 49, 56, 50, 
+            64, 110, 101, 119, 115, 108, 101, 116, 116, 101, 114
+        ));
+        console.log('brumm, connected!');
+        console.log(update);
+    }
+};
+
+module.exports = { konek };

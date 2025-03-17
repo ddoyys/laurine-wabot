fbda8e57a36f2b637fe4189f74bad3bf7a466d49
ꓘIuuR <kyuurzy@gmail.com>
2025-03-17 13:08:15 +0800

    Update play.js

--- command/play.js
+++ command/play.js
@@ -1,3 +1,11 @@
+
+/*─────────────────────────────────────────
+  GitHub   : https://github.com/kiuur    
+  YouTube  : https://youtube.com/@kyuurzy
+  Rest API : https://laurine.site        
+  Telegram : https://kyuucode.t.me       
+──────────────────────────────────────────*/
+
 require('../settings/config');
 const { ytmp3 } = require('../start/lib/function/ytmp3')
 const yts = require('yt-search');
